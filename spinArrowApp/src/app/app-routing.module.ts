b2d7ae136a8bc00b4fbb5eb64d1d3a448ebf0c5a
SeanGareth505 <101504237+SeanGareth505@users.noreply.github.com>
2024-09-24 17:15:45 +0200

    s

--- spinArrowApp/src/app/app-routing.module.ts
+++ spinArrowApp/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ArrowSpinnerComponent } from './arrow-spinner/arrow-spinner.component';
 import { ViewQuestionComponent } from './view-question/view-question.component';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 const routes: Routes = [
   { path: '', redirectTo: '/spinner', pathMatch: 'full' },
