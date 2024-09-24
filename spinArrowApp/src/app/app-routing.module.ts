c41f34f3855039284cd6fc877385c0a3a5b8fdc3
SeanGareth505 <101504237+SeanGareth505@users.noreply.github.com>
2024-09-24 16:34:41 +0200

    Update app-routing.module.ts

--- spinArrowApp/src/app/app-routing.module.ts
+++ spinArrowApp/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ArrowSpinnerComponent } from './arrow-spinner/arrow-spinner.component';
 import { ViewQuestionComponent } from './view-question/view-question.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 const routes: Routes = [
   { path: '', redirectTo: '/spinner', pathMatch: 'full' },
@@ -10,7 +11,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
 })
 export class AppRoutingModule { }
